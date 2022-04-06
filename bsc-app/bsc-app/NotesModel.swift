9fadc00442ce9d4831618428192c55b16ce9362e
PonomarevIvanIvanovich <93083971+PonomarevIvanIvanovich@users.noreply.github.com>
2022-04-06 22:20:05 +0400

    Выполнил задание 2

---
+++ bsc-app/bsc-app/NotesModel.swift
@@ -0,0 +1,24 @@
+//
+//  NotesModel.swift
+//  bsc-app
+//
+//  Created by Иван Пономарев on 01.04.2022.
+//
+
+import Foundation
+
+final class NotesModel: Codable {
+
+    var header: String
+    var notesText: String
+    var dateNotes: String?
+    var isEmpt: Bool {
+        return header.isEmpty || notesText.isEmpty
+    }
+
+    init(header: String, notesText: String, dateNotes: String) {
+        self.header = header
+        self.notesText = notesText
+        self.dateNotes = dateNotes
+    }
+}
---
+++ bsc-app/bsc-app/NotesStorage.swift
@@ -0,0 +1,26 @@
+//
+//  DateStoregу.swift
+//  bsc-app
+//
+//  Created by Иван Пономарев on 05.04.2022.
+//
+
+import Foundation
+
+final class NotesStorage {
+    static var notesModel: NotesModel? {
+        get {
+            guard let savedData = UserDefaults.standard.object(forKey: "saveData") as? Data,
+                let decodedModel = try? JSONDecoder().decode(NotesModel.self, from: savedData) else { return nil }
+            return decodedModel
+        }
+        set {
+            let defaults = UserDefaults.standard
+            if let notesModel = newValue {
+                if let saveData = try? JSONEncoder().encode(notesModel) {
+                    defaults.set(saveData, forKey: "saveData")
+                }
+            }
+        }
+    }
+}
